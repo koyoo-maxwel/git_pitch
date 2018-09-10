8d197ca8bec7f71bfcf626b2bb1f8900b8e61741
koyoo  maxwel <koyoomaxwel@gmail.com>
2018-09-10 17:28:50 +0300

    creating registration form

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,6 +2,7 @@ from flask import render_template,redirect,url_for, flash,request
 from ..models import User
 from .forms import RegistrationForm
 from .forms import LoginForm,RegistrationForm
+from flask_login import login_user,logout_user,login_required
 from .. import db
 from . import auth
 
@@ -31,3 +32,10 @@ def login():
 
     title = "pitch login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
