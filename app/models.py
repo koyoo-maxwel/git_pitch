bd76f42077f90132460a0e603c05d2c382634849
koyoo  maxwel <koyoomaxwel@gmail.com>
2018-09-10 18:15:52 +0300

    success sigin

--- app/models.py
+++ app/models.py
@@ -30,7 +30,7 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(255))
-    pass_secure = db.Column(db.String(255))
+    # pass_secure = db.Column(db.String(255))
 
     def __repr__(self):
         return f'User {self.username}'
@@ -39,10 +39,10 @@ class User(UserMixin,db.Model):
 def password(self):
     raise AttributeError('You cannot read the password attribute')
 
-@password.setter
-def password(self, password):
-    self.pass_secure = generate_password_hash(password)
+# @password.setter
+def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
 
 
 def verify_password(self,password):
-    return check_password_hash(self.pass_secure,password)
+    return check_password_hash(self.pass_hash,password)
