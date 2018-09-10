e54b345c9ed719e4b3079ab0cc9dca219b0730a4
koyoo  maxwel <koyoomaxwel@gmail.com>
2018-09-10 14:13:53 +0300

    added unittest

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,13 @@
 from flask import render_template
-from app import app
+from . import main
 
-# Views
-@app.route('/')
+
+@main.route('/')
+
 def index():
-
-    '''
+    """
     View root page function that returns the index page and its data
-    '''
+    """
+    title = "Welcome to  your best Pitches || pitch it now !"
+    return render_template('index.html', title = title)
 
-    return render_template('index.html', title = title, h2)
-
-    
