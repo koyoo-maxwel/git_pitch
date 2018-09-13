2b44333d3732532368301b16cc5751955705a441
koyoo  maxwel <koyoomaxwel@gmail.com>
2018-09-13 16:12:37 +0300

    app is now working well

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager,Server
 from app.models import User,Category,Pitch
 from flask_migrate import Migrate , MigrateCommand
 
-app = create_app('')
+app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
